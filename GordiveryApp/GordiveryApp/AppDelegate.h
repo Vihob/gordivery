e5dbdca7b916d97b67e12ce90a01d41b769139b3
vfernandez <victor.fernandez@mobivery.com>
2012-10-26 17:18:42 +0200

    Adding Empty iPhone project
    Signed-off-by: vfernandez <victor.fernandez@mobivery.com>

---
+++ GordiveryApp/GordiveryApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GordiveryApp
+//
+//  Created by Victor L. Fernandez Rodrigo on 26/10/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GordiveryApp/GordiveryApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GordiveryApp
+//
+//  Created by Victor L. Fernandez Rodrigo on 26/10/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
